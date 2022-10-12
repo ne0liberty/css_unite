05be695c3e27e56f26e1bee02973561c7b381ae1
ne0liberty <gustafkusuma@gmail.com>
2022-10-12 16:33:03 +0700

    new view order

--- pages/forms/view_order.php
+++ pages/forms/view_order.php
@@ -188,7 +188,7 @@
           </div>
           <!-- /.card-body -->
           <div class="card-footer">
-            <button type="submit" name="submit" class="btn btn-primary">Update</button>
+            <button type="submit" name="submit-incoming" class="btn btn-primary">Update</button>
           </div>
         </div>
         <!-- /.card -->
@@ -250,8 +250,8 @@
           </div>
           <!-- /.card-body -->
           <div class="card-footer">
-            <button type="submit" name="submit" class="btn btn-primary">Update</button>
-            <button type="submit" name="submit-shipmentorder" class="btn btn-default">Shipment Order</button>
+            <button type="submit" name="submit-outgoing" class="btn btn-primary">Update</button>
+            <button type="submit" name="submit_shipmentorder" class="btn btn-default">Shipment Order</button>
           </div>
         </div>
         <!-- /.card -->
@@ -373,21 +373,20 @@
                   $awb_out = $_POST['awb_out'];
                   $awb_out_date = $_POST['awb_out_date'];
                   $core_cond = $_POST['core_cond'];
-        
-                  // update data ke database
+                  
+                  
                   $update1 = mysqli_query($koneksi, "UPDATE master_order SET note='$note', repair_tat='$repair_tat', inbound='$inbound', awb_in='$awb_in', awb_date='$awb_date', eta='$eta', gr_date='$gr_date', serial_number='$serial_number', serv_batch='$serv_batch', date_store='$date_store', pn_out='$pn_out', sn_out='$sn_out', core_batch='$core_batch', shipment_order_date='$shipment_order_date', awb_out='$awb_out', awb_out_date='$awb_out_date', core_cond='$core_cond' WHERE id_order='$id'");                 
                   $update2 = mysqli_query($koneksi, "UPDATE master_order SET serv_status=IF(awb_in='','Waiting AWB',IF(gr_date='','SHIPPED',IF(date_store='','Waiting Inspect','CLOSED')));");
+                  
 
 
                   //ini untuk menampilkan alert berhasil dan redirect ke halaman index
                   echo "<script>alert('data berhasil disimpan.');window.location='index.php?page=data_order';</script>";
-                 }       
-    ?>
-    <?php
-        include('conf/conn.php');
+                 };       
+  
 				
                 //melakukan pengecekan jika button submit diklik maka akan menjalankan perintah simpan dibawah ini
-                 if (isset($_POST['submit-shipmentorder'])) {
+                 if (isset($_POST['submit_shipmentorder'])) {
                   //menampung data dari inputan
 
                   $note = $_POST['note'];
@@ -417,10 +416,8 @@
 
                   //ini untuk menampilkan alert berhasil dan redirect ke halaman index
                   echo "<script>alert('data berhasil disimpan.');window.location='index.php?page=shipment_order_single&id=".$id."';</script>";
-                 }       
-      ?>
-      <?php
-        include('conf/conn.php');
+                 };       
+ 
 				
                 //melakukan pengecekan jika button submit diklik maka akan menjalankan perintah simpan dibawah ini
                  if (isset($_POST['submit_cost'])) {
@@ -438,7 +435,7 @@
 
                   //ini untuk menampilkan alert berhasil dan redirect ke halaman index
                   echo "<script>window.open('pages/forms/ca_sheet.php?id=".$id."', '_blank');</script>";
-                 }       
+                 };       
       ?>
   </section>
   <script>
