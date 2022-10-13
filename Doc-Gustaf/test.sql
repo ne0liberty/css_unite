c8ca95a93d2979f0f4b14e9d632ae8f866247a1b
ne0liberty <gustafkusuma@gmail.com>
2022-10-13 10:27:22 +0700

    test sql status

--- Doc-Gustaf/test.sql
+++ Doc-Gustaf/test.sql
@@ -1,5 +1,5 @@
 UPDATE 
-    master_order 
+    master_order
 SET 
     tat_po = CASE WHEN serv_status <>'' THEN CURDATE()-entry_date ELSE date_store-entry_date
     sla_vendor = CASE WHEN CURDATE()-ca_app_date<=repair_tat THEN 'ONTIME' ELSE 'OVERDUE'
--- Doc-Gustaf/test1.sql
+++ Doc-Gustaf/test1.sql
@@ -1,10 +1,9 @@
 UPDATE master_order
-SET serv_status
+SET serv_status 
 CASE 
-
-    WHEN awb_in ='' THEN 'Waiting AWB' 
-    WHEN gr_date ='' THEN 'SHIPPED'
-    WHEN date_store ='' THEN 'Waiting Inspect'
+    WHEN awb_in = '' THEN 'Waiting AWB' 
+    WHEN gr_date = '' THEN 'SHIPPED'
+    WHEN date_store = '' THEN 'Waiting Inspect'
     ELSE 'CLOSED'
 
 END;
