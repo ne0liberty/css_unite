3cfe3faa00bf268b27e9bf100cd7ad3dd325f667
ne0liberty <gustafkusuma@gmail.com>
2022-10-13 16:32:50 +0700

    NEW

--- Doc-Gustaf/test.sql
+++ Doc-Gustaf/test.sql
@@ -4,3 +4,6 @@ SET
     tat_po = CASE WHEN serv_status <>'' THEN CURDATE()-entry_date ELSE date_store-entry_date
     sla_vendor = CASE WHEN CURDATE()-ca_app_date<=repair_tat THEN 'ONTIME' ELSE 'OVERDUE'
 END;
+
+
+UPDATE master_order SET invoice='testdariterminal' WHERE po_number=340011288;
--- Doc-Gustaf/update_stat.sql
+++ Doc-Gustaf/update_stat.sql
@@ -1,4 +1,4 @@
 UPDATE master_order SET 
 tat_po = IF(serv_status<>'',CURDATE()-entry_date,date_store-entry_date),
-sla_vendor = IF(ca_date='','ONTIME',IF(ca_app_date<>'',IF(CURRENT_DATE()-ca_app_date<=repair_tat ,'ONTIME','OVERDUE'),'ONTIME')),
-tat_gr = IF(eta='','',IF(gr_date='',CURRENT_DATE()-eta,gr_date-eta));
+sla_vendor = IF(ca_date='','ONTIME',IF(ca_app_date<>'',IF(CURDATE()-ca_app_date<=repair_tat ,'ONTIME','OVERDUE'),'ONTIME')),
+tat_gr = IF(eta='','',IF(gr_date='',CURDATE()-eta,gr_date-eta));
