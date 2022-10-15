104f311a3ab90c02adb1674186baf33fec7cf553
ne0liberty <gustafkusuma@gmail.com>
2022-10-15 13:45:01 +0700

    UPDATE SET CASE WHEN FIX

--- Doc-Gustaf/test.sql
+++ Doc-Gustaf/test.sql
@@ -1,9 +1,7 @@
-UPDATE 
-    master_order
+-- Active: 1661187206419@@127.0.0.1@3306@css_order
+UPDATE master_order
 SET 
-    tat_po = CASE WHEN serv_status <>'' THEN CURDATE()-entry_date ELSE date_store-entry_date
-    sla_vendor = CASE WHEN CURDATE()-ca_app_date<=repair_tat THEN 'ONTIME' ELSE 'OVERDUE'
-END;
+tat_po = CASE WHEN serv_status <>'' THEN CURDATE()-entry_date ELSE date_store-entry_date END,
+sla_vendor = CASE WHEN CURDATE()-ca_app_date<=repair_tat THEN 'ONTIME' ELSE 'OVERDUE' END;
 
 
-UPDATE master_order SET invoice='testdariterminal' WHERE po_number=340011288;
