283edd222f174ce7ed522bfceca4101e23f7ffff
ne0liberty <gustafkusuma@gmail.com>
2022-10-17 21:07:58 +0700

    update status

--- pages/forms/view_order.php
+++ pages/forms/view_order.php
@@ -379,28 +379,28 @@
                   
                   //update error running at ubuntu
                   $update2 = mysqli_query($koneksi, "UPDATE master_order SET serv_status=
-                             IF(req_scheme='exchange',
-                             IF(awb_in='','NEED AWB IN',
-                             IF(gr_date='','SERV SHIPPED',
-                             IF(date_store='','NEED INSPECT',
-                             IF(sn_out='','NEED CORE',
-                             IF(shipment_order_date='','NEED SO',
-                             IF(awb_out='','NEED AWB OUT',
-                             IF(invoice='','NEED REPAIR QUOTE',
-                             IF(ca_app_date='','NEED REPAIR APPROVAL',
-                             IF(payment_ref='','NEED PAYMENT',
-                             'CLOSED'))))))))),
-                             IF(sn_out='','NEED CORE',
-                             IF(shipment_order_date='','NEED SO',
-                             IF(awb_out='','NEED AWB OUT',
-                             IF(invoice='','NEED REPAIR QUOTE',
-                             IF(ca_app_date='','NEED REPAIR APPROVAL',
-                             IF(payment_ref='','NEED PAYMENT',
-                             IF(awb_in='','NEED AWB IN',
-                             IF(gr_date='','SERV SHIPPED',
-                             IF(date_store='','NEED INSPECT',
-                             'CLOSED')))))))))
-                             );");   
+                           IF(req_scheme='exchange',
+                           IF(awb_in='','NEED AWB IN',
+                           IF(gr_date='','SERV SHIPPED',
+                           IF(date_store='','NEED INSPECT',
+                           IF(sn_out='','NEED CORE',
+                           IF(shipment_order_date='','NEED SO',
+                           IF(awb_out='','NEED AWB OUT',
+                           IF(invoice='','NEED REPAIR QUOTE',
+                           IF(ca_app_date='','NEED REPAIR APPROVAL',
+                           IF(payment_ref='','NEED PAYMENT',
+                           'CLOSED'))))))))),
+                           IF(sn_out='','NEED CORE',
+                           IF(shipment_order_date='','NEED SO',
+                           IF(awb_out='','NEED AWB OUT',
+                           IF(invoice='','NEED REPAIR QUOTE',
+                           IF(ca_app_date='','NEED REPAIR APPROVAL',
+                           IF(payment_ref='','NEED PAYMENT',
+                           IF(awb_in='','NEED AWB IN',
+                           IF(gr_date='','SERV SHIPPED',
+                           IF(date_store='','NEED INSPECT',
+                           'CLOSED')))))))))
+                           );");   
 
                   //ini untuk menampilkan alert berhasil dan redirect ke halaman index
                   echo "<script>alert('data berhasil disimpan.');window.location='index.php?page=data_order';</script>";
