d8a6a34c2870c3b7c4287782244902c216bd09ce
ne0liberty <gustafkusuma@gmail.com>
2022-10-21 21:46:07 +0700

    alert revision

--- pages/tables/api_cancel_order.php
+++ pages/tables/api_cancel_order.php
@@ -6,5 +6,5 @@
 			$datas = mysqli_query($koneksi, "UPDATE master_order SET serv_status='CANCEL' WHERE id_order='$id'") or die(mysqli_error($koneksi));
 
 			//alert dan redirect ke index.php
-			echo "<script>alert('order cancelled.');window.location='../../index.php?page=data_order';</script>";
+			echo "<script>alert('Order has been Cancelled.');window.location='../../index.php?page=data_order';</script>";
 	?>
