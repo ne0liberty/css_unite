e38b9b6f4c8990741aa75340200cd98ae459255b
ne0liberty <gustafkusuma@gmail.com>
2022-10-27 04:56:23 +0700

    created by

--- Doc-Gustaf/update_stat.sql
+++ Doc-Gustaf/update_stat.sql
@@ -1,4 +1,4 @@
--- Active: 1661187206419@@127.0.0.1@3306@css_order
+-- Active: 1661199485383@@127.0.0.1@3306@css_order
 UPDATE master_order SET 
 tat_po = IF(serv_status<>'',CURDATE()-entry_date,date_store-entry_date),
 sla_vendor = IF(ca_date='','ONTIME',IF(ca_app_date<>'',IF(CURDATE()-ca_app_date<=repair_tat ,'ONTIME','OVERDUE'),'ONTIME')),
