03b8c0e670422ec177cccc7a4f26365022be0e06
ne0liberty <gustafkusuma@gmail.com>
2022-11-02 13:47:50 +0700

    datediff fix

--- Doc-Gustaf/update_stat.sql
+++ Doc-Gustaf/update_stat.sql
@@ -1,5 +1,5 @@
--- Active: 1661187206419@@127.0.0.1@3306@css_order
+-- Active: 1665550077115@@127.0.0.1@3306@css_order
 UPDATE master_order SET 
-tat_po = IF(date_store='',CURDATE()-entry_date,date_store-entry_date),
-sla_vendor = IF(ca_date='','ONTIME',IF(ca_app_date<>'',IF(CURDATE()-ca_app_date<=repair_tat ,'ONTIME','OVERDUE'),'ONTIME')),
-tat_gr = IF(eta='','',IF(gr_date='',CURDATE()-eta,gr_date-eta));
+tat_po = IF(date_store='',DATEDIFF(CURDATE(), entry_date),date_store-entry_date),
+sla_vendor = IF(ca_date='','ONTIME',IF(ca_app_date<>'',IF(DATEDIFF(CURDATE(), ca_app_date)<=repair_tat ,'ONTIME','OVERDUE'),'ONTIME')),
+tat_gr = IF(eta='','',IF(gr_date='',DATEDIFF(CURDATE(), eta),gr_date-eta));
