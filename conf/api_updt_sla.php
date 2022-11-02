efd74dc5d0dae6d29d0123d76c73dc1e921e6030
ne0liberty <gustafkusuma@gmail.com>
2022-11-02 13:48:46 +0700

    update sla fix

--- conf/api_updt_sla.php
+++ conf/api_updt_sla.php
@@ -3,8 +3,8 @@
 include('conn.php');
 
 $updt = mysqli_query($koneksi, "UPDATE master_order SET 
-tat_po = IF(date_store='',CURDATE()-entry_date,date_store-entry_date),
-sla_vendor = IF(ca_date='','ONTIME',IF(ca_app_date<>'',IF(CURDATE()-ca_app_date<=repair_tat ,'ONTIME','OVERDUE'),'ONTIME')),
-tat_gr = IF(eta='','',IF(gr_date='',CURDATE()-eta,gr_date-eta));");
+tat_po = IF(date_store='',DATEDIFF(CURDATE(), entry_date),date_store-entry_date),
+sla_vendor = IF(ca_date='','ONTIME',IF(ca_app_date<>'',IF(DATEDIFF(CURDATE(), ca_app_date)<=repair_tat ,'ONTIME','OVERDUE'),'ONTIME')),
+tat_gr = IF(eta='','',IF(gr_date='',DATEDIFF(CURDATE(), eta),gr_date-eta));");
 
 ?>
