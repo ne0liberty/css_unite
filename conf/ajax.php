5ae72c746a1c6d5ac4a48bf62039d5788e91f2d5
ne0liberty <gustafkusuma@gmail.com>
2022-11-19 21:42:28 +0700

    desc to description in pn database

--- conf/ajax.php
+++ conf/ajax.php
@@ -8,7 +8,7 @@ $part_number = $_GET['part_number'];
 //mengambil data
 $query = mysqli_query($koneksi2, "select * from pn_database where part_number='$part_number'");
 $desc_lookup = mysqli_fetch_array($query);
-$data = array('desc'      =>  @$desc_lookup['desc'],);
+$data = array('description'      =>  @$desc_lookup['description'],);
 
 //tampil data
 echo json_encode($data);
