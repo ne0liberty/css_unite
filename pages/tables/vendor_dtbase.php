e2fdf2030a3887693303f62dd3fbdd94d92d02f9
ne0liberty <gustafkusuma@gmail.com>
2022-11-22 11:09:47 +0700

    vendor database new

--- pages/tables/vendor_dtbase.php
+++ pages/tables/vendor_dtbase.php
@@ -26,75 +26,61 @@ $nama = ucwords($_SESSION['NAME']);
     <section class="content">
     <form action="" method="post" role="form">
       <div class="container-fluid">
-        <!-- Tittle  -->
-        <div class="card card-primary">
-          <div class="card-header">
-            <h3 class="card-title"><i class="fas fa-tag"></i>
-            Shipment Order</h3>
-            <div class="card-tools">
-              
+        <!-- left column -->
+        <div class="col-md-6">
+          <!-- Tittle  -->
+          <div class="card card-default">
+            <div class="card-header">
+              <h3 class="card-title">Vendor List</h3>
+              <div class="card-tools">
+                    <ul class="nav nav-pills ml-auto">
+                     <li class="nav-item">
+                       <a class="btn btn-primary float-right" href="" data-toggle="modal" data-target="#submit-vendor"><i class="fas fa-plus"></i> Add item</a>
+                     </li>
+                   </ul>
+              </div>
             </div>
-          </div>
-          <!-- /.card-header -->
-          <div class="card-body">
-            <div class="row">
-              <div class="col-md-6">
-                <div class="form-group">
-                  <label>Vendor</label>
-                    <select class="form-control select2bs4" id='vendor' name="vendor" onchange="isi_otomatis();loadData()" style="width: 100%;">
-                      <option value="">- Select Vendor -</option>
-                      <?php 
-                            include('conf/conn.php');
-                            
-                            $datas3 = mysqli_query($koneksi, "SELECT * FROM vendor_database") or die (mysqli_error($koneksi));
-                            while($data_vendor = mysqli_fetch_array($datas3))  {
-                              
-                              echo "<option>$data_vendor[vendor]</option>";
-                          }
-                      ?>
-                  </select>
-                </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+               <div class="form-group">
+                 <label>Vendor</label>
+                   <select class="form-control select2bs4" id='vendor' name="vendor" onchange="isi_otomatis()" style="width: 100%;">
+                     <option value="">- Select Vendor -</option>
+                     <?php 
+                           include('conf/conn.php');
+                           
+                           $datas3 = mysqli_query($koneksi, "SELECT * FROM vendor_database") or die (mysqli_error($koneksi));
+                           while($data_vendor = mysqli_fetch_array($datas3))  {
+                             
+                             echo "<option>$data_vendor[vendor]</option>";
+                         }
+                     ?>
+                 </select>
+               </div>
 
-                <script>
-                function getElements()
-                {
-                document.getElementById("address1").value = document.getElementById("address").value;
-                }
-                
-                </script>
-                
-                <div class="form-group">
-                  <div class="form-check">
-                    <input class="form-check-input" type="radio" name="address" value="" onclick="getElements()" checked>
-                    <label class="form-check-label">Address 1</label>
-                    <textarea class="form-control" rows="7" id="address1" name="address"></textarea>
-                  </div>
-                    
-                  <div class="form-check">
-                    <input class="form-check-input" type="radio" name="address" value="" onclick="getElements()">
-                    <label class="form-check-label">Address 2</label>
-                    <textarea class="form-control" rows="7" id="address2" name="address_2"></textarea>
-                  </div>
-                    
-                </div>
-                
-              </div>
-              <!-- /.col -->
-              <div class="col-md-6">
-                
-              </div>
-              <!-- /.col -->
+               <script>
+               function getElements()
+               {
+               document.getElementById("address1").value = document.getElementById("address").value;
+               }
+               
+               </script>
+         
+               <div class="form-group">   
+                   <label>Address 1</label>
+                   <textarea class="form-control" rows="7" id="address1" name="address" readonly></textarea>
+               </div> 
+               <div class="form-group">
+                 <label>Address 2</label>
+                 <textarea class="form-control" rows="7" id="address2" name="address_2" readonly></textarea>
+               </div>
             </div>
-            <!-- /.row -->
-          
+            <!-- /.card-body -->
+            <div class="card-footer">
+            </div>
           </div>
-          <!-- /.card-body -->
-          <div class="card-footer">
-            
-          </div>
+          <!-- /.card -->
         </div>
-        <!-- /.card -->
-        
       </div>
     </form>
       
