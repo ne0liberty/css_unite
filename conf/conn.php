c5332ddfc2812e94c98c8cb48454592df7675a2c
ne0liberty <gustafkusuma@gmail.com>
2022-12-12 04:46:57 +0700

    test connect

--- conf/conn.php
+++ conf/conn.php
@@ -8,5 +8,14 @@ $koneksi    = mysqli_connect($server, $username, $password, $db); //pastikan uru
 
 //untuk cek jika koneksi gagal ke database
 if(mysqli_connect_errno()) {
-	echo "Koneksi gagal : ".mysqli_connect_error();
+	echo "SQL koneksi gagal : ".mysqli_connect_error();
+}else{
+ $query  = "SELECT * FROM admins WHERE username = 'gustaf_123'";
+ $result = mysqli_query($koneksi,$query);
+ 
+ while($row = mysqli_fetch_array($result)) {
+    echo $row['name']; 
+	}
 }
+
+?>
